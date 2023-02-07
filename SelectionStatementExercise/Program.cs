a9f7a7a28a220719fe5cc565a59fd1be08d0dbf5
Tom Settle <tom.k.settle@gmail.com>
2023-02-07 17:17:36 -0600

    Mod 3, Selection Statements, Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Let's come up with a radom number f between 1 and 1000
+            var r = new Random();
+            int favNumber = r.Next(1, 500);
+
+            // Invite the user to guess it
+            int userGuess = 0;
+            int numAttempts = 0;
+            do 
+            {
+                Console.WriteLine("Guess my number (1 to 500)...");
+                userGuess = int.Parse(Console.ReadLine());
+                ++numAttempts;
+                if (userGuess < favNumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (userGuess > favNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+            } while (userGuess != favNumber);
+            Console.WriteLine($"Congratulations on a correct guess. You escaped the endless loop in {numAttempts} tries!");
         }
     }
 }
